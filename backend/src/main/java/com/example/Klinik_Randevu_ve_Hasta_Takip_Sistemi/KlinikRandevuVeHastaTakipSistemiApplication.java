369233a32b5d6f1c070001d03dd3fb9740bfb96f
Erkim Berk Ünsal <erkimberk@hotmail.com>
2026-04-04 09:57:43 +0300

    initialize spring boot with postgresql and jpa configuration and react

---
+++ backend/src/main/java/com/example/Klinik_Randevu_ve_Hasta_Takip_Sistemi/KlinikRandevuVeHastaTakipSistemiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Klinik_Randevu_ve_Hasta_Takip_Sistemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlinikRandevuVeHastaTakipSistemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlinikRandevuVeHastaTakipSistemiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/Klinik_Randevu_ve_Hasta_Takip_Sistemi/KlinikRandevuVeHastaTakipSistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Klinik_Randevu_ve_Hasta_Takip_Sistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlinikRandevuVeHastaTakipSistemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
